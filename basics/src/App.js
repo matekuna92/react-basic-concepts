5f4a7a8db542df20b1e985deaec12d77e4eea4cb
matekuna <kuna.mate@parlament.hu>
2022-01-07 13:03:49 +0100

    basic element rendering

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+const user = {
+	firstName: 'Harper',
+	lastName: 'Perez'
+};
+
+const element = (
+	<h1>Hello, {formatName(user)}</h1>	
+)
+
+function formatName(user) {
+	return `${user.firstName} ${user.lastName}`;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+		element
+    );
 }
 
 export default App;

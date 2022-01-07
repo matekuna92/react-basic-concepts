80a5fdb66b2f91e04ed03b7aa7d22c201dc43fea
matekuna <kuna.mate@parlament.hu>
2022-01-07 13:23:09 +0100

    function component

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 
-const user = {
+/* const user = {
 	firstName: 'Harper',
 	lastName: 'Perez'
 };
@@ -12,11 +12,24 @@ const element = (
 
 function formatName(user) {
 	return `${user.firstName} ${user.lastName}`;
+} */
+
+
+// function components are the simplest way to write a component:
+function Welcome(props) {
+	return <h1>Hello, {props.name}</h1>
 }
+
+// React element can represent user-defined components. React passes JSX attributes and children to the component as "props" object
+// const element = <Welcome name="Sara" />
 
 function App() {
     return (
-		element
+		<div>
+			<Welcome name="Sara" />
+			<Welcome name="Cahal" />
+			<Welcome name="Edite" />
+		</div>
     );
 }
 

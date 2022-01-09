278bd1cd91d89e92b1bcb53efdfb0c5850273e19
matekuna92 <kunamate66@gmail.com>
2022-01-09 21:25:17 +0100

    basic <ul> list

--- basics/src/index.js
+++ basics/src/index.js
@@ -246,6 +246,19 @@ class WarningPage extends React.Component {
   }
 }
 
+// LISTS AND KEYS - RENDERING MULTIPLE COMPONENTS
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>{number}</li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
 
 ReactDOM.render(
   <React.StrictMode>
@@ -254,6 +267,7 @@ ReactDOM.render(
     <Toggle />
     <LoginControl />
     <WarningPage />
+    <NumberList numbers={numbers} />
   </React.StrictMode>,
   document.getElementById('root')
 );
